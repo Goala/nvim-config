23f8d72e53bfeb352317136309966030875c118b
Goala <max_stenke@yahoo.de>
2025-02-09 22:52:21 +0100

    Update theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,21 @@
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("catppuccin-mocha")
+-- 	end,
+-- }
+
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparency = true,
+			},
+		})
+		vim.cmd("colorscheme rose-pine-moon")
+	end,
+}
