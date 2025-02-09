910ad8b04d74d522ce9b56b624e5ebebac94b023
Goala <max_stenke@yahoo.de>
2025-02-09 21:38:12 +0100

    Ignore node_modules in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,6 +20,9 @@ return {
             require("telescope.themes").get_dropdown({}),
           },
         },
+        file_ignore_patterns = {
+          "node_modules",
+        },
       })
       require("telescope").load_extension("ui-select")
     end,
