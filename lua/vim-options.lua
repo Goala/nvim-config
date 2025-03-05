30d369576f6d7875787dbc34bb92e6661b7cb03d
Max Stenke <max.stenke@haufe-akademie.de>
2025-03-05 11:30:49 +0100

    Set case options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
