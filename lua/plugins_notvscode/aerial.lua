bfd132fcd4a1b0045fac7105a0deeacdbd5c8db4
Goala <max_stenke@yahoo.de>
2025-03-24 22:28:27 +0100

    Set aerial default direction

--- lua/plugins_notvscode/aerial.lua
+++ lua/plugins_notvscode/aerial.lua
@@ -1,9 +1,13 @@
 return {
-  "stevearc/aerial.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("aerial").setup()
+	"stevearc/aerial.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("aerial").setup({
+			layout = {
+				default_direction = "prefer_left",
+			},
+		})
 
-    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle! left<CR>")
-  end,
+		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle! left<CR>")
+	end,
 }
