f3cf0e722910c3e9577382a760ecb28c1bf37386
Max Stenke <max.stenke@haufe-akademie.de>
2025-03-28 10:56:59 +0100

    Add keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,3 +11,6 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set({ "n", "v" }, "Y", '"+y')
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
