253507c18e5548a28a7974dd0c3e16d400c6ef86
Max Stenke <max.stenke@haufe-akademie.de>
2025-04-02 15:29:11 +0200

    Update lualine config

--- lua/plugins_notvscode/lualine.lua
+++ lua/plugins_notvscode/lualine.lua
@@ -1,21 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        globalstatus = true,
-      },
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-          },
-          {
-            "aerial",
-          },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				globalstatus = true,
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 4,
+					},
+					{
+						"aerial",
+					},
+				},
+			},
+		})
+	end,
 }
