1dc2ba971343f72622b50808550678b781c32d8e
Max Stenke <max.stenke@haufe-akademie.de>
2025-04-03 15:20:28 +0200

    Remove default ts lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
 
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
+				ensure_installed = { "lua_ls" },
 				handlers = {
 					function(server_name)
 						require("lspconfig")[server_name].setup({
