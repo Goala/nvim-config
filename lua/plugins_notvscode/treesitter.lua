7dfe4800e76d06be29fcfed9259455b0f08983fa
Max Stenke <max.stenke@haufe-akademie.de>
2025-04-03 15:20:47 +0200

    Add codefolding with treesitter

--- lua/plugins_notvscode/treesitter.lua
+++ lua/plugins_notvscode/treesitter.lua
@@ -8,5 +8,13 @@ return {
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
+
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.opt.foldcolumn = "0"
+		vim.opt.foldtext = ""
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+		vim.opt.foldnestmax = 4
 	end,
 }
