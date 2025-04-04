0d773e72bcffe5724c2f07aea87983f45fa321f8
Max Stenke <max.stenke@haufe-akademie.de>
2025-04-04 16:27:57 +0200

    Remove gitblame and use more gitsigns

--- lua/plugins_notvscode/gitsigns.lua
+++ lua/plugins_notvscode/gitsigns.lua
@@ -2,6 +2,10 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	config = function()
-		require("gitsigns").setup()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_formatter = "<author> • <author_time:%d-%m-%Y %H:%M:%S> • <summary>",
+			current_line_blame_opts = { delay = 500 },
+		})
 	end,
 }
