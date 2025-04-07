e186511f838ed8ece698fc2e326d8eb2259432d9
Goala <max_stenke@yahoo.de>
2025-04-07 21:42:54 +0200

    Try gitsigns numhl

--- lua/plugins_notvscode/gitsigns.lua
+++ lua/plugins_notvscode/gitsigns.lua
@@ -3,6 +3,8 @@ return {
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	config = function()
 		require("gitsigns").setup({
+			signcolumn = false,
+			numhl = true,
 			current_line_blame = true,
 			current_line_blame_formatter = "<author> • <author_time:%d-%m-%Y %H:%M:%S> • <summary>",
 			current_line_blame_opts = { delay = 500 },
