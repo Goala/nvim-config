9e0e205ae91f95cacef245911afe4aa7aaa34b6a
Goala <max_stenke@yahoo.de>
2025-04-09 21:29:23 +0200

    Set qs colors

--- lua/plugins_notvscode/quick-scope.lua
+++ lua/plugins_notvscode/quick-scope.lua
@@ -1,4 +1,8 @@
 return {
 	"unblevable/quick-scope",
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+	config = function()
+		vim.cmd.highlight("QuickScopePrimary guifg='#ff9e64' gui=underline ctermfg=155 cterm=underline")
+		vim.cmd.highlight("QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
+	end,
 }
