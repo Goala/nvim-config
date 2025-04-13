0f8952cff9a798873207f5ff20140bd1f3ce4595
Goala <max_stenke@yahoo.de>
2025-04-13 15:03:37 +0200

    Switch back to mini surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,19 +1,23 @@
 return {
-	"kylechui/nvim-surround",
-	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-	config = function()
-		require("nvim-surround").setup()
+	"echasnovski/mini.surround",
+	opts = {
+		custom_surroundings = nil,
+		highlight_duration = 500,
+		mappings = {
+			add = "sa", -- Add surrounding in Normal and Visual modes
+			delete = "sd", -- Delete surrounding
+			find = "sf", -- Find surrounding (to the right)
+			find_left = "sF", -- Find surrounding (to the left)
+			highlight = "sh", -- Highlight surrounding
+			replace = "sr", -- Replace surrounding
+			update_n_lines = "sn", -- Update `n_lines
 
-        -- insert = "<C-g>s",
-        -- insert_line = "<C-g>S",
-        -- normal = "ys",
-        -- normal_cur = "yss",
-        -- normal_line = "yS",
-        -- normal_cur_line = "ySS",
-        -- visual = "S",
-        -- visual_line = "gS",
-        -- delete = "ds",
-        -- change = "cs",
-        -- change_line = "cS",
-	end,
+			suffix_last = "l", -- Suffix to search with "prev" method
+			suffix_next = "n", -- Suffix to search with "next" method
+		},
+		n_lines = 20,
+		respect_selection_type = false,
+		search_method = "cover",
+		silent = false,
+	},
 }
