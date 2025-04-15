7d1a60dcee443d4d204f19f563d23013922e735e
Goala <max_stenke@yahoo.de>
2025-04-15 21:56:50 +0200

    Set auto undofiles

--- lua/plugins_notvscode/undotree.lua
+++ lua/plugins_notvscode/undotree.lua
@@ -3,5 +3,7 @@ return {
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	config = function()
 		vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+		vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+		vim.opt.undofile = true
 	end,
 }
