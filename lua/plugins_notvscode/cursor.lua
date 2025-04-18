769702ad6eed86d67126c27382dca434419cec01
Goala <max_stenke@yahoo.de>
2025-04-18 22:05:41 +0200

    Tweak smear

--- lua/plugins_notvscode/cursor.lua
+++ lua/plugins_notvscode/cursor.lua
@@ -1,5 +1,11 @@
 return {
 	"sphamba/smear-cursor.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		stiffness = 0.7,
+		trailing_stiffness = 0.5,
+		stiffness_insert_mode = 0.6,
+		trailing_stiffness_insert_mode = 0.6,
+		distance_stop_animating = 0.2,
+	},
 }
