56e7c978c90f0791d72031ebb1382970dedcb57c
Goala <max_stenke@yahoo.de>
2025-04-18 22:45:06 +0200

    Fix autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,20 +2,21 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		config = function()
-			-- local autopairs = require("nvim-autopairs") -- import nvim-autopairs
+		config = true,
+		-- config = function()
+		-- local autopairs = require("nvim-autopairs") -- import nvim-autopairs
 
-			-- setup autopairs
-			-- autopairs.setup({
-			-- 	check_ts = true, -- treesitter enabled
-			-- })
-			-- -- import nvim-autopairs completion functionality
-			-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			-- -- import nvim-cmp plugin (completions plugin)
-			-- local cmp = require("cmp")
-			-- -- make autopairs and completion work together
-			-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
+		-- setup autopairs
+		-- autopairs.setup({
+		-- 	check_ts = true, -- treesitter enabled
+		-- })
+		-- -- import nvim-autopairs completion functionality
+		-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- -- import nvim-cmp plugin (completions plugin)
+		-- local cmp = require("cmp")
+		-- -- make autopairs and completion work together
+		-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		-- end,
 	},
 	{
 		"windwp/nvim-ts-autotag",
