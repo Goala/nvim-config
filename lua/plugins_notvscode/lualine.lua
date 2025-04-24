7a928a7c10b0991d80d15cfd4c8c973c9a403c91
Max Stenke <max.stenke@haufe-akademie.de>
2025-04-24 10:40:57 +0200

    Update lualine

--- lua/plugins_notvscode/lualine.lua
+++ lua/plugins_notvscode/lualine.lua
@@ -5,12 +5,29 @@ return {
 		require("lualine").setup({
 			options = {
 				globalstatus = true,
+				theme = "horizon",
 			},
 			sections = {
 				lualine_c = {
 					{
 						"filename",
-						path = 3,
+						path = 1,
+					},
+				},
+				lualine_z = {
+					{
+						"lsp_status",
+						icon = "", -- f013
+						symbols = {
+							-- Standard unicode symbols to cycle through for LSP progress:
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							-- Standard unicode symbol for when LSP is done:
+							done = "✓",
+							-- Delimiter inserted between LSP names:
+							separator = " ",
+						},
+						-- List of LSP names to ignore (e.g., `null-ls`):
+						ignore_lsp = {},
 					},
 				},
 			},
