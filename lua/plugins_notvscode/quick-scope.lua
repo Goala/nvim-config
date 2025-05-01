b00804e6f6c733fa4fe1d70cd38f8a5979c35e9f
Goala <max_stenke@yahoo.de>
2025-05-01 23:51:02 +0200

    Try quickstop blacklists

--- lua/plugins_notvscode/quick-scope.lua
+++ lua/plugins_notvscode/quick-scope.lua
@@ -4,5 +4,6 @@ return {
 	config = function()
 		vim.cmd.highlight("QuickScopePrimary guifg='#ff9e64' gui=underline ctermfg=155 cterm=underline")
 		vim.cmd.highlight("QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
+		vim.cmd([[let g:qs_filetype_blacklist = ['undotree', "neo-tree"] ]])
 	end,
 }
