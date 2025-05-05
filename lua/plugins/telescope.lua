a39acd87432912edc82fd87365bad7c8e0fb28a0
Goala <max_stenke@yahoo.de>
2025-05-05 21:39:58 +0200

    Ad fzf native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
 		event = "VeryLazy",
 		config = function()
