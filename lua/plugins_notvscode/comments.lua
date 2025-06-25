0afa6079f789a38ac7c753904e6e50dcf162310c
Goala <max_stenke@yahoo.de>
2025-06-25 15:57:45 +0200

    Add todo comments

--- lua/plugins_notvscode/comments.lua
+++ lua/plugins_notvscode/comments.lua
@@ -1,6 +1,17 @@
 return {
-  "folke/ts-comments.nvim",
-  opts = {},
-  event = "VeryLazy",
-  enabled = vim.fn.has("nvim-0.10.0") == 1,
+	{
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
